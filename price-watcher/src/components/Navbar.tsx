720f7fb6991f0b2e4d9af9d85b2074720b235bda
Raffaella <131981162+SabatinoRaffaella@users.noreply.github.com>
2026-08-25 14:29:44 +0200

    Navigation Bar + Component

---
+++ price-watcher/src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import pricewatcherLogo from '../assets/pricewatcher.svg'
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo"><img src={pricewatcherLogo}/></div>
+      <div className="navbar-links">
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
